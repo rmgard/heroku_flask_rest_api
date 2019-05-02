4664755e98b3f3df1844b25540996d832403509b
rmgard <root@C254707.wks.anadarko.com>
2019-05-02 12:32:36 -0500

    Removed run.py. The table creation is now in app.py. This was a pain figure out... I don't think he addresses this in the lesson.

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import os
 
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
+from flask_jwt_extended import JWTManager 
 
-from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -16,13 +15,26 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity) # /auth
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWTManager(app) 
+
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1: # Instead of hard-coding, you should read from a config file or a db
+        return {'is_admin': True}
+    return {'is_admin': False}
+
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>') 
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     from db import db
--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
+from flask_jwt_extended import jwt_required
 from models.item import ItemModel
 
 
@@ -16,7 +16,7 @@ class Item(Resource):
             help="Every item needs a store id!"
             )
 
-    @jwt_required()
+    @jwt_required
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
@@ -39,7 +39,12 @@ class Item(Resource):
 
         return item.json(), 201
 
+    @jwt_required
     def delete(self, name):
+        claims = get_jwt_claims()
+        if not claims['is_admin']:
+            return {'message': 'Admin priviledge required.'}, 401
+
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
